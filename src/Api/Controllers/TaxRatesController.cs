4ca8d4e27389f5b1a1ea3d2f3229fc483032c2c5
Pedro Freitas de Souza <pefreita@microsoft.com>
2021-03-05 11:23:19 +0100

    Better name for the controller

--- src/Api/Controllers/TaxRatesController.cs
+++ src/Api/Controllers/TaxRatesController.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System;
 using Taxes.Application.TaxSchedules.Queries.GetTaxSchedules;
 using Taxes.Application.TaxSchedules.Queries.GetTaxScheduleSummary;
 
 namespace Taxes.Api.Controllers
 {
-    [Route("api/taxes/")]
-    public class TaxesController : ApiControllerBase
+    [Route("api/taxrates/")]
+    public class TaxRatesController : ApiControllerBase
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TaxScheduleResponse>>> GetAllAsync([FromQuery] GetTaxesSummaryQuery query)
